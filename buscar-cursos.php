0c6976a32dd0cdca950028a4c0ca5cf1bc586d44
Matheus Viana <matheusvcv@yahoo.com.br>
2022-08-31 11:16:37 -0300

    Baixado e instalado os pacotes guzzlehttp/guzzle, symfony/dom-crawler e symfony/css-selector. Criado script que percorre o html e retorna todos os nomes de cursos deefinidos na url definida

---
+++ buscar-cursos.php
@@ -0,0 +1,19 @@
+<?php 
+
+require 'vendor/autoload.php';
+require 'src/buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = New Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = New Crawler();
+
+$buscador = New Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+	
+	echo $curso . PHP_EOL;
+}

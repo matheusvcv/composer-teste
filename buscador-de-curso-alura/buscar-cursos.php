3e38b1b1b3a4799c943d6cb0c7fc8389275604cf
Matheus Viana <matheusvcv@yahoo.com.br>
2022-09-02 22:43:45 -0300

    Adicionado arquivo bin e corrigindo bugs

--- buscador-de-curso-alura/buscar-cursos.php
+++ buscador-de-curso-alura/buscar-cursos.php
@@ -1,3 +1,5 @@
+#!/usr/bin/env php
+
 <?php 
 
 require 'vendor/autoload.php';
@@ -14,5 +16,5 @@ $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
 	
-	echo exibeMensagem($curso);
+	exibeMensagem($curso);
 }
